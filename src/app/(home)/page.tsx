3c1c7229fcc536a2047582496e2943811fbf33bc
nakmiers <diwas118151@gmail.com>
2025-02-27 20:15:45 +0700

    3

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,72 @@
-import React from 'react'
+'use client'
 
-function page() {
-  return <div>page</div>
+import History from '@/components/History'
+import { DateRangePicker } from '@/components/ui/DateRangePicker'
+import { Separator } from '@/components/ui/separator'
+import Wallets from '@/components/Wallets'
+import { differenceInDays } from 'date-fns'
+import moment from 'moment-timezone'
+import { useCallback, useEffect, useState } from 'react'
+import toast from 'react-hot-toast'
+
+function HomePage() {
+  // states
+  const [dateRange, setDateRange] = useState<{ from: Date; to: Date }>({
+    from: moment().startOf('month').toDate(),
+    to: moment().toDate(),
+  })
+
+  const [loading, setLoading] = useState<boolean>(false)
+
+  // get overview
+  const getOverview = useCallback(async () => {
+    // start loading
+    setLoading(true)
+
+    try {
+    } catch (err: any) {
+      console.log(err)
+    } finally {
+      // stop loading
+      setLoading(false)
+    }
+  }, [])
+
+  // initially get stats
+  useEffect(() => {
+    getOverview()
+  }, [getOverview])
+
+  return (
+    <div className="">
+      <div className="flex items-center justify-end px-21/2 py-4 md:px-21">
+        <DateRangePicker
+          initialDateFrom={dateRange.from}
+          initialDateTo={dateRange.to}
+          showCompare={false}
+          onUpdate={values => {
+            const { from, to } = values.range
+
+            if (!from || !to) return
+            if (differenceInDays(to, from) > +90) {
+              toast.error(`The selected date range is too large. Max allowed range is ${90} days!`)
+              return
+            }
+
+            setDateRange({ from, to })
+          }}
+        />
+      </div>
+
+      <Wallets />
+
+      <Separator className="my-8 h-0" />
+
+      <History />
+
+      <div className="pt-32" />
+    </div>
+  )
 }
 
-export default page
+export default HomePage
